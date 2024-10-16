58519090fd35fb3af980170768b7059caf9c58a8
miguel gonzalez tapia <gonzalez21ip@gmail.comgit config --global user.name>
2024-10-16 16:16:25 -0300

    Las redes del footer quedan ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// script.js
+
 
 function openInstagramAndRefresh() {
     // Abre Instagram en una nueva pestaña
@@ -8,5 +8,27 @@ function openInstagramAndRefresh() {
     location.reload();
 }
 
-// Puedes mantener el resto del código para el efecto del botón aquí
+
+
+function openWhatsAppAndRefresh() {
+    // Abre WhatsApp en una nueva pestaña
+    window.open('https://wa.me/+56985607542?text=Hola,%20necesito%20su%20servicio', '_blank');
+    
+    // Recarga la página actual
+    location.reload();
+}
+
+
+
+function openFacebookAndRefresh() {
+    // Abre Facebook en una nueva pestaña
+    window.open('https://www.facebook.com/profile.php?id=61566129607635&mibextid=LQQJ4d', '_blank');
+    
+    // Recarga la página actual
+    location.reload();
+}
+
+
+
+
 
