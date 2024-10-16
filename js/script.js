66e5b0185c79d3d61b2afba76d4cc6f6db697c7c
miguel gonzalez tapia <gonzalez21ip@gmail.comgit config --global user.name>
2024-10-16 16:03:16 -0300

    probando boton insta 1

--- js/script.js
+++ js/script.js
@@ -8,12 +8,19 @@ button.addEventListener('mousedown', () => {
     button.style.transform = 'translate(2px, 2px)'; // Mover el botón al hacer clic
 });
 
-// Restaura la posición original al soltar el clic
-button.addEventListener('mouseup', () => {
+// Función para restaurar la posición original del botón
+function resetButton() {
     button.style.transform = 'translate(0, 0)'; // Restaurar la posición original
+}
+
+// Añade un evento de clic para abrir el enlace
+button.addEventListener('click', () => {
+    // Restaura la posición del botón después de un breve momento
+    setTimeout(resetButton, 150); // Ajusta el tiempo según sea necesario (150 ms aquí)
+    
+    // Abre el enlace de Instagram en una nueva pestaña
+    window.open('https://www.instagram.com/gruas.myc/', '_blank');
 });
 
 // También puedes agregar un evento de mouseleave para restaurar si se sale del botón
-button.addEventListener('mouseleave', () => {
-    button.style.transform = 'translate(0, 0)'; // Restaurar la posición original
-});
+button.addEventListener('mouseleave', resetButton);
