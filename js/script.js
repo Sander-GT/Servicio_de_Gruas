286b19eb060feb4f92c98ce8e0a0c78d9319e375
miguel gonzalez tapia <gonzalez21ip@gmail.comgit config --global user.name>
2024-10-17 21:28:44 -0300

    Avance de web 90%

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 function openInstagramAndRefresh() {
     // Abre Instagram en una nueva pestaña
     window.open('https://www.instagram.com/gruas.myc/', '_blank');
-    
+
     // Recarga la página actual
     location.reload();
 }
@@ -11,22 +11,30 @@ function openInstagramAndRefresh() {
 
 
 function openWhatsAppAndRefresh() {
-    // Abre WhatsApp en una nueva pestaña
-    window.open('https://wa.me/+56985607542?text=Hola,%20necesito%20su%20servicio', '_blank');
-    
+    // Mensaje predefinido para la solicitud del servicio
+    const message = "Hola, estoy interesado en solicitar el servicio de grúa. ¿Podrían brindarme más información?";
+
+    // Abre WhatsApp con el mensaje predefinido en una nueva pestaña
+    window.open(`https://wa.me/+56985607542?text=${encodeURIComponent(message)}`, '_blank');
+
     // Recarga la página actual
     location.reload();
 }
+
 
 
 
 function openFacebookAndRefresh() {
     // Abre Facebook en una nueva pestaña
     window.open('https://www.facebook.com/profile.php?id=61566129607635&mibextid=LQQJ4d', '_blank');
-    
+
     // Recarga la página actual
     location.reload();
 }
+
+
+
+
 
 
 
